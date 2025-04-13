2c1fecdab1d1d5327480c5da1b0ba35407fd3727
codaMW <codamw011@gmail.com>
2025-04-13 16:07:31 +0000

    try

--- submission/04.sh
+++ submission/04.sh
@@ -1,5 +1,27 @@
 # Create a CLTV script with a timestamp of 1495584032 and public key below:
 # publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 
-1495584032 OP_CHECKLOCKTIMEVERIFY OP_DROP 02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277 OP_CHECKSIG
+
+# Locktime in decimal
+locktime=1495584032
+
+# Convert locktime to little-endian hex (4 bytes)
+locktime_le=$(printf '%08x\n' $locktime | sed 's/../& /g' | awk '{print $4$3$2$1}')
+
+publicKey="02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277"
+
+# Derive pubKeyHash (RIPEMD160(SHA256(pubKey)))
+pubKeyHash=$(echo -n $publicKey | xxd -r -p | openssl sha256 -binary | openssl ripemd160 | xxd -p -c 999)
+
+# <4-byte locktime> OP_CHECKLOCKTIMEVERIFY OP_DROP OP_DUP OP_HASH160 <pubKeyHash> OP_EQUALVERIFY OP_CHECKSIG
+scriptPubKey="04${locktime_le}b17576a914${pubKeyHash}88ac"
+
+echo "ScriptPubKey (hex): $scriptPubKey"
+
+#decoded=$(bitcoin-cli -regtest decodescript $scriptPubKey)
+
+# Extract P2SH address
+#p2sh_address=$(echo $decoded | jq -r '.p2sh')
+
+#echo "P2SH address: $p2sh_address"
 
