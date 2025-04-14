be6588eb523a4aa3c2440e6514106a4b9574e496
codaMW <codamw011@gmail.com>
2025-04-14 03:50:44 +0000

    need to fix the hash function

--- submission/04.sh
+++ submission/04.sh
@@ -4,10 +4,14 @@ publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 
 locktime=1495584032
 
-locktimeHex=$(printf '%x' $locktime)
 
-cltvScript="${locktimeHex} OP_CHECKLOCKTIMEVERIFY OP_DROP ${publicKey} OP_CHECKSIG"
+locktime_hex=$(printf '%08x' $locktime)
 
-echo $cltvScript
+locktime_le=$(echo $locktime_hex | sed 's/../& /g' | awk '{for(i=NF;i>0;i--) printf $i}' )
 
+
+pubkeyhash=$(echo -n "$publicKey" | xxd -r -p | openssl sha256 -binary | openssl ripemd160 -binary | xxd -p -c 256)
+
+
+echo "04${locktime_le}b17576a914${pubkeyhash}88ac"
 
